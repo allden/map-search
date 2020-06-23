02bf79bb1ede1ea885e4093a2493cf991cadb99f
allden <30358515+allden@users.noreply.github.com>
2020-06-23 04:27:34 +0300

    Updates

---
+++ precache-manifest.6d4c9b2de01e6154f11ff20253b273df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94693d039be399bc6c744d280b74ea6e",
+    "url": "/map-search/index.html"
+  },
+  {
+    "revision": "65cb29a62206774b67fa",
+    "url": "/map-search/static/css/2.d4e1a0f7.chunk.css"
+  },
+  {
+    "revision": "506139fcc3e7e27079a8",
+    "url": "/map-search/static/css/main.89988bda.chunk.css"
+  },
+  {
+    "revision": "65cb29a62206774b67fa",
+    "url": "/map-search/static/js/2.09e41032.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/map-search/static/js/2.09e41032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "506139fcc3e7e27079a8",
+    "url": "/map-search/static/js/main.551cb428.chunk.js"
+  },
+  {
+    "revision": "5c8306f1ca13d05f6ca8",
+    "url": "/map-search/static/js/runtime-main.95abc8c8.js"
+  },
+  {
+    "revision": "1c48ce08d5e06aa1a1f82616d11cbfd2",
+    "url": "/map-search/static/media/cloud-compressed.1c48ce08.jpg"
+  }
+]);
